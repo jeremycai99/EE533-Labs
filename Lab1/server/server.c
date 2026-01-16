924f8a55a07d9bdf9ebcec9656e9e908e5dc203e
jeremycai99 <cyjcyj1999@gmail.com>
2026-01-15 22:14:58 -0800

    Update src

--- Lab1/server/server.c
+++ Lab1/server/server.c
@@ -1,6 +1,7 @@
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
