325bb460881042334a4493dcc526df21b9b5ed32
jeremycai99 <cyjcyj1999@gmail.com>
2026-01-15 22:16:46 -0800

    Update src

--- Lab1/server/server.c
+++ Lab1/server/server.c
@@ -2,6 +2,7 @@
    The port number is passed as an argument */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
