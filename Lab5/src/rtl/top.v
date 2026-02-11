a3ff2e1ae2f360db017439168bee9ece8874b8b1
jeremycai99 <cyjcyj1999@gmail.com>
2026-02-11 11:09:49 -0800

    Update src

---
+++ Lab5/src/rtl/top.v
@@ -0,0 +1,19 @@
+`ifndef TOP_V
+`define TOP_V
+
+`include "define.v"
+
+`include "i_mem.v"
+`include "d_mem.v"
+`include "cpu.v"
+
+module top (
+    input wire clk,
+    input wire rst_n
+);
+
+// Instantiate instruction memory
+
+
+
+`endif // TOP_V
