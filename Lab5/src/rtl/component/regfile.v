5ed61da1458bb3e92ff4f0be9e7055a763233b87
jeremycai99 <cyjcyj1999@gmail.com>
2026-02-14 20:48:48 -0800

    Fix synthesis isses in src

--- Lab5/src/rtl/component/regfile.v
+++ Lab5/src/rtl/component/regfile.v
@@ -19,28 +19,26 @@ module regfile(
     input [`REG_ADDR_WIDTH-1:0] waddr, // Destination register address
     input [`REG_DATA_WIDTH-1:0] wdata, // Data to write to the register
 
-    output reg [`REG_DATA_WIDTH-1:0] r0data, // Data read from source register 1
-    output reg [`REG_DATA_WIDTH-1:0] r1data, // Data read from source register 2
+    output wire [`REG_DATA_WIDTH-1:0] r0data, // Data read from source register 1
+    output wire [`REG_DATA_WIDTH-1:0] r1data, // Data read from source register 2
     
     // ILA probe signals for debugging
     input [`REG_ADDR_WIDTH-1:0] ila_cpu_reg_addr, // ILA probe address input
-    output reg [`REG_DATA_WIDTH-1:0] ila_cpu_reg_data // ILA probe data output
+    output wire [`REG_DATA_WIDTH-1:0] ila_cpu_reg_data // ILA probe data output
 );
 
-reg [`REG_DATA_WIDTH-1:0] regfile [0:(1<<`REG_ADDR_WIDTH)-1]; // Register file array
+reg [`REG_DATA_WIDTH-1:0] regs [0:(1<<`REG_ADDR_WIDTH)-1]; // Register file array
 
 // Register read logic: combinational read
-always @(*) begin
     // do not need to qualify with waddr != 0 since Arm doesn't restrict writes to x0
-    r0data = regfile[r0addr]; // Register x0 is hardwired to 0
-    r1data = regfile[r1addr]; // Register x0 is hardwired to 0
-    ila_cpu_reg_data = regfile[ila_cpu_reg_addr]; // ILA probe data output
-end
+assign r0data = regs[r0addr]; // Register x0 is hardwired to 0
+assign r1data = regs[r1addr]; // Register x0 is hardwired to 0
+assign ila_cpu_reg_data = regs[ila_cpu_reg_addr]; // ILA probe data output
 
 // Register write logic: synchronous write on clock edge
 always @(posedge clk) begin
     if (wena) begin // do not need to qualify with waddr != 0 since Arm doesn't restrict writes to x0
-        regfile[waddr] <= wdata;
+        regs[waddr] <= wdata;
     end
 end
 endmodule
