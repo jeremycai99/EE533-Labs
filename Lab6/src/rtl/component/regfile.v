3674f3257ba78778d92801273c8e9a879fa58dd8
jeremycai99 <cyjcyj1999@gmail.com>
2026-02-18 14:25:21 -0800

    Update regfile to three read ports design

--- Lab6/src/rtl/component/regfile.v
+++ Lab6/src/rtl/component/regfile.v
@@ -17,6 +17,7 @@ module regfile(
     input clk, // Clock signal. No reset signal needed
     input [`REG_ADDR_WIDTH-1:0] r0addr, // Source register 1 address
     input [`REG_ADDR_WIDTH-1:0] r1addr, // Source register 2 address
+    input [`REG_ADDR_WIDTH-1:0] r2addr, // Source register 3 address (for multiply instructions)
 
     input wena, // Write enable signal
     input [`REG_ADDR_WIDTH-1:0] waddr, // Destination register address
@@ -24,7 +25,7 @@ module regfile(
 
     output wire [`REG_DATA_WIDTH-1:0] r0data, // Data read from source register 1
     output wire [`REG_DATA_WIDTH-1:0] r1data, // Data read from source register 2
-    
+    output wire [`REG_DATA_WIDTH-1:0] r2data, // Data read from source register 3 (for multiply instructions)
     // ILA probe signals for debugging
     input [`REG_ADDR_WIDTH-1:0] ila_cpu_reg_addr, // ILA probe address input
     output wire [`REG_DATA_WIDTH-1:0] ila_cpu_reg_data // ILA probe data output
@@ -36,8 +37,10 @@ reg [`REG_DATA_WIDTH-1:0] regs [0:(1<<`REG_ADDR_WIDTH)-1]; // Register file arra
 // do not need to qualify with waddr != 0 since Arm doesn't restrict writes to x0
 // Implement register forwarding: if the current instruction is writing to a register that is being read
 // forward the write data instead of reading from the register file
-assign r0data = (wena && waddr == r0addr) ? wdata : regs[r0addr]; // Register x0 is hardwired to 0
-assign r1data = (wena && waddr == r1addr) ? wdata : regs[r1addr]; // Register x0 is hardwired to 0
+assign r0data = (wena && waddr == r0addr) ? wdata : regs[r0addr]; 
+assign r1data = (wena && waddr == r1addr) ? wdata : regs[r1addr]; 
+assign r2data = (wena && waddr == r2addr) ? wdata : regs[r2addr]; 
+
 assign ila_cpu_reg_data = regs[ila_cpu_reg_addr]; // ILA probe data output
 
 // Register write logic: synchronous write on clock edge
